26cc72c8963becc1c4ec4fe7e5d4c5043c33a1ec
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-11-04 09:05:41 +0100

    #22  #11

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,15 +1,43 @@
+import styles from "./styles/main.css";
+import { json } from "@remix-run/node"; 
+import { useLoaderData } from "@remix-run/react";
 
 export const meta = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "The Ocean Cleanup" },
+    { name: "description", content: "The Ocean Cleanup is a non-profit organization dedicated to developing advanced technologies to rid the world's oceans of plastic pollution. Join our mission to clean up and protect our oceans for a sustainable future." },
   ];
 };
 
+export async function loader() {
+  const res = await fetch("https://fdnd-toc-api.netlify.app/total");
+  return json(await res.json());
+}
+
 export default function Index() {
+  const loaderData = useLoaderData(); 
+
+  console.log(loaderData); 
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>test</h1>
-    </div>
+    <section>
+        <header><h1>Dashboard</h1></header>
+        <article><h2>{loaderData.totals.debris_extracted_total}</h2></article>
+        <article><h2>{loaderData.totals.debris_extracted_last_30d}</h2></article>
+        <article><h2>Trash removed in total</h2></article>
+        <article>map</article>
+        <article><h2>Introduction</h2></article>
+        <article><h2>Cirkeldiagram</h2></article>
+        <article><h2>Cirkeldiagram</h2></article>
+        <article><h2>Trash removed month</h2></article>
+        <article>system status</article>
+    </section>
   );
 }
+
+
+
+
+export const links = () => [
+  { rel: "stylesheet", href: styles },
+];
